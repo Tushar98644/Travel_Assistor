f8add1690828ad66f2e584b0ca427e2aae32965d
Tushar98644 <b.toshar@iitg.ac.in>
2023-05-20 17:06:37 +0530

    globe_map added

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,10 +1,55 @@
-import Map from 'react-map-gl'
+'use client'
+
+import Map, { Marker, Popup } from 'react-map-gl'
 import 'mapbox-gl/dist/mapbox-gl.css'
+import { Room } from '@material-ui/icons'
+import { useEffect, useState } from 'react'
 
 const Home_page = () => {
-  return ( 
-    <div>home_page</div>
-   );
+  const [viewport, setViewport] = useState({
+    width: '100vw',
+    height: '100vh',
+    latitude: 0,
+    longitude: 0,
+    zoom: 10
+  });
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords;
+        setViewport((prevViewport) => ({
+          ...prevViewport,
+          latitude,
+          longitude
+        }));
+      },
+      (error) => {
+        console.error('Error getting user location:', error);
+      }
+    );
+  }, []);
+  return (
+    <div>
+      <Map projection={'globe'} mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX}
+        mapStyle='mapbox://styles/tushar-evil/clhvuyagf005o01qsadhsbixd'
+        style={{ width: "100vw", height: "100vh" }}>
+        <Marker
+          latitude={viewport.latitude}
+          longitude={viewport.longitude}>
+          <Room style={{ fontSize: "40px", color: "red" }} />
+        </Marker>
+        <Popup
+          latitude={viewport.latitude}
+          longitude={viewport.longitude}  
+          closeButton={true}
+          closeOnClick={false}
+          anchor="left">
+            <div></div>
+        </Popup>
+      </Map>
+    </div>
+  );
 }
- 
+
 export default Home_page;
